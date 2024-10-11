0de9c3ba407a3b5d363b4e0f8f0d5c3b751004e0
STUSSY-449 <972871710@qq.com>
2024-10-11 18:50:19 +0800

    first commit

---
+++ nn/extra_modules/block.py
@@ -0,0 +1,405 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+import math
+import numpy as np
+from einops import rearrange
+from ..modules.conv import Conv, DWConv, DSConv, RepConv, GhostConv, autopad
+from ..modules.block import *
+from .orepa import *
+from .RFAConv import *
+from ultralytics.utils.torch_utils import make_divisible
+from timm.layers import trunc_normal_
+from timm.layers import CondConv2d
+
+__all__ = ['DyHeadBlock', 'ChannelAttention_MHSFPN',  'C2f_Dynamic', ]
+
+def autopad(k, p=None, d=1):  # kernel, padding, dilation
+    """Pad to 'same' shape outputs."""
+    if d > 1:
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k]  # actual kernel-size
+    if p is None:
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k]  # auto-pad
+    return p
+
+
+try:
+    from mmcv.cnn import build_activation_layer, build_norm_layer
+    from mmcv.ops.modulated_deform_conv import ModulatedDeformConv2d
+    from mmengine.model import constant_init, normal_init
+except ImportError as e:
+    pass
+
+def _make_divisible(v, divisor, min_value=None):
+    if min_value is None:
+        min_value = divisor
+    new_v = max(min_value, int(v + divisor / 2) // divisor * divisor)
+    # Make sure that round down does not go down by more than 10%.
+    if new_v < 0.9 * v:
+        new_v += divisor
+    return new_v
+
+
+class swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=False):
+        super(h_swish, self).__init__()
+        self.inplace = inplace
+
+    def forward(self, x):
+        return x * F.relu6(x + 3.0, inplace=self.inplace) / 6.0
+
+
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True, h_max=1):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+        self.h_max = h_max
+
+    def forward(self, x):
+        return self.relu(x + 3) * self.h_max / 6
+
+
+class DyReLU(nn.Module):
+    def __init__(self, inp, reduction=4, lambda_a=1.0, K2=True, use_bias=True, use_spatial=False,
+                 init_a=[1.0, 0.0], init_b=[0.0, 0.0]):
+        super(DyReLU, self).__init__()
+        self.oup = inp
+        self.lambda_a = lambda_a * 2
+        self.K2 = K2
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+
+        self.use_bias = use_bias
+        if K2:
+            self.exp = 4 if use_bias else 2
+        else:
+            self.exp = 2 if use_bias else 1
+        self.init_a = init_a
+        self.init_b = init_b
+
+        # determine squeeze
+        if reduction == 4:
+            squeeze = inp // reduction
+        else:
+            squeeze = _make_divisible(inp // reduction, 4)
+        # print('reduction: {}, squeeze: {}/{}'.format(reduction, inp, squeeze))
+        # print('init_a: {}, init_b: {}'.format(self.init_a, self.init_b))
+
+        self.fc = nn.Sequential(
+            nn.Linear(inp, squeeze),
+            nn.ReLU(inplace=True),
+            nn.Linear(squeeze, self.oup * self.exp),
+            h_sigmoid()
+        )
+        if use_spatial:
+            self.spa = nn.Sequential(
+                nn.Conv2d(inp, 1, kernel_size=1),
+                nn.BatchNorm2d(1),
+            )
+        else:
+            self.spa = None
+
+    def forward(self, x):
+        if isinstance(x, list):
+            x_in = x[0]
+            x_out = x[1]
+        else:
+            x_in = x
+            x_out = x
+        b, c, h, w = x_in.size()
+        y = self.avg_pool(x_in).view(b, c)
+        y = self.fc(y).view(b, self.oup * self.exp, 1, 1)
+        if self.exp == 4:
+            a1, b1, a2, b2 = torch.split(y, self.oup, dim=1)
+            a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+            a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
+
+            b1 = b1 - 0.5 + self.init_b[0]
+            b2 = b2 - 0.5 + self.init_b[1]
+            out = torch.max(x_out * a1 + b1, x_out * a2 + b2)
+        elif self.exp == 2:
+            if self.use_bias:  # bias but not PL
+                a1, b1 = torch.split(y, self.oup, dim=1)
+                a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+                b1 = b1 - 0.5 + self.init_b[0]
+                out = x_out * a1 + b1
+
+            else:
+                a1, a2 = torch.split(y, self.oup, dim=1)
+                a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+                a2 = (a2 - 0.5) * self.lambda_a + self.init_a[1]
+                out = torch.max(x_out * a1, x_out * a2)
+
+        elif self.exp == 1:
+            a1 = y
+            a1 = (a1 - 0.5) * self.lambda_a + self.init_a[0]  # 1.0
+            out = x_out * a1
+
+        if self.spa:
+            ys = self.spa(x_in).view(b, -1)
+            ys = F.softmax(ys, dim=1).view(b, 1, h, w) * h * w
+            ys = F.hardtanh(ys, 0, 3, inplace=True)/3
+            out = out * ys
+
+        return out
+
+class DyDCNv2(nn.Module):
+    """ModulatedDeformConv2d with normalization layer used in DyHead.
+    This module cannot be configured with `conv_cfg=dict(type='DCNv2')`
+    because DyHead calculates offset and mask from middle-level feature.
+    Args:
+        in_channels (int): Number of input channels.
+        out_channels (int): Number of output channels.
+        stride (int | tuple[int], optional): Stride of the convolution.
+            Default: 1.
+        norm_cfg (dict, optional): Config dict for normalization layer.
+            Default: dict(type='GN', num_groups=16, requires_grad=True).
+    """
+
+    def __init__(self,
+                 in_channels,
+                 out_channels,
+                 stride=1,
+                 norm_cfg=dict(type='GN', num_groups=16, requires_grad=True)):
+        super().__init__()
+        self.with_norm = norm_cfg is not None
+        bias = not self.with_norm
+        self.conv = ModulatedDeformConv2d(
+            in_channels, out_channels, 3, stride=stride, padding=1, bias=bias)
+        if self.with_norm:
+            self.norm = build_norm_layer(norm_cfg, out_channels)[1]
+
+    def forward(self, x, offset, mask):
+        """Forward function."""
+        x = self.conv(x.contiguous(), offset, mask)
+        if self.with_norm:
+            x = self.norm(x)
+        return x
+
+
+class DyHeadBlock(nn.Module):
+    """DyHead Block with three types of attention.
+    HSigmoid arguments in default act_cfg follow official code, not paper.
+    https://github.com/microsoft/DynamicHead/blob/master/dyhead/dyrelu.py
+    """
+
+    def __init__(self,
+                 in_channels,
+                 norm_type='GN',
+                 zero_init_offset=True,
+                 act_cfg=dict(type='HSigmoid', bias=3.0, divisor=6.0)):
+        super().__init__()
+        self.zero_init_offset = zero_init_offset
+        # (offset_x, offset_y, mask) * kernel_size_y * kernel_size_x
+        self.offset_and_mask_dim = 3 * 3 * 3
+        self.offset_dim = 2 * 3 * 3
+
+        if norm_type == 'GN':
+            norm_dict = dict(type='GN', num_groups=16, requires_grad=True)
+        elif norm_type == 'BN':
+            norm_dict = dict(type='BN', requires_grad=True)
+
+        self.spatial_conv_high = DyDCNv2(in_channels, in_channels, norm_cfg=norm_dict)
+        self.spatial_conv_mid = DyDCNv2(in_channels, in_channels)
+        self.spatial_conv_low = DyDCNv2(in_channels, in_channels, stride=2)
+        self.spatial_conv_offset = nn.Conv2d(
+            in_channels, self.offset_and_mask_dim, 3, padding=1)
+        self.scale_attn_module = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1), nn.Conv2d(in_channels, 1, 1),
+            nn.ReLU(inplace=True), build_activation_layer(act_cfg))
+        self.task_attn_module = DyReLU(in_channels)
+        self._init_weights()
+
+    def _init_weights(self):
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                normal_init(m, 0, 0.01)
+        if self.zero_init_offset:
+            constant_init(self.spatial_conv_offset, 0)
+
+    def forward(self, x):
+        """Forward function."""
+        outs = []
+        for level in range(len(x)):
+            # calculate offset and mask of DCNv2 from middle-level feature
+            offset_and_mask = self.spatial_conv_offset(x[level])
+            offset = offset_and_mask[:, :self.offset_dim, :, :]
+            mask = offset_and_mask[:, self.offset_dim:, :, :].sigmoid()
+
+            mid_feat = self.spatial_conv_mid(x[level], offset, mask)
+            sum_feat = mid_feat * self.scale_attn_module(mid_feat)
+            summed_levels = 1
+            if level > 0:
+                low_feat = self.spatial_conv_low(x[level - 1], offset, mask)
+                sum_feat += low_feat * self.scale_attn_module(low_feat)
+                summed_levels += 1
+            if level < len(x) - 1:
+                # this upsample order is weird, but faster than natural order
+                # https://github.com/microsoft/DynamicHead/issues/25
+                high_feat = F.interpolate(
+                    self.spatial_conv_high(x[level + 1], offset, mask),
+                    size=x[level].shape[-2:],
+                    mode='bilinear',
+                    align_corners=True)
+                sum_feat += high_feat * self.scale_attn_module(high_feat)
+                summed_levels += 1
+            outs.append(self.task_attn_module(sum_feat / summed_levels))
+
+        return outs
+
+
+
+
+
+
+
+class ChannelAttention_MHSFPN(nn.Module):
+    def __init__(self, in_planes, ratio = 4, flag=True):
+        super(ChannelAttention_MHSFPN, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
+
+        self.conv1 = nn.Conv2d(in_planes, in_planes // ratio, 1, bias=False)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(in_planes // ratio, in_planes, 1, bias=False)
+        self.flag = flag
+        self.sigmoid = nn.Sigmoid()
+
+        nn.init.xavier_uniform_(self.conv1.weight)
+        nn.init.xavier_uniform_(self.conv2.weight)
+
+    def forward(self, x):
+        avg_out = self.conv2(self.relu(self.conv1(self.avg_pool(x))))
+        max_out = self.conv2(self.relu(self.conv1(self.max_pool(x))))
+        out = avg_out + max_out
+        return self.sigmoid(out) * x if self.flag else self.sigmoid(out)
+
+
+
+class CA_MHSFPN(nn.Module):
+    def __init__(self, inp, reduction=8, flag=True):
+        super(CA_MHSFPN, self).__init__()
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        mip = max(8, inp // reduction)
+        self.conv1 = nn.Conv2d(inp, mip, kernel_size=1, stride=1, padding=0)
+        self.bn1 = nn.BatchNorm2d(mip)
+        self.act = h_swish()
+        self.conv_h = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+        self.conv_w = nn.Conv2d(mip, inp, kernel_size=1, stride=1, padding=0)
+
+        self.flag = flag
+
+    def forward(self, x):
+        n, c, h, w = x.size()
+        x_h = self.pool_h(x)
+        x_w = self.pool_w(x).permute(0, 1, 3, 2)
+
+        y = torch.cat([x_h, x_w], dim=2)
+        y = self.conv1(y)
+        y = self.bn1(y)
+        y = self.act(y)
+
+        x_h, x_w = torch.split(y, [h, w], dim=2)
+        x_w = x_w.permute(0, 1, 3, 2)
+
+        a_h = self.conv_h(x_h).sigmoid()
+        a_w = self.conv_w(x_w).sigmoid()
+
+        out = a_w * a_h
+        return x * out if self.flag else out
+
+
+
+
+
+
+
+
+
+
+
+class DynamicConv_Single(nn.Module):
+    """ Dynamic Conv layer
+    """
+    def __init__(self, in_features, out_features, kernel_size=1, stride=1, padding='', dilation=1,
+                 groups=1, bias=False, num_experts=4):
+        super().__init__()
+        self.routing = nn.Linear(in_features, num_experts)
+        self.cond_conv = CondConv2d(in_features, out_features, kernel_size, stride, padding, dilation,
+                 groups, bias, num_experts)
+        
+    def forward(self, x):
+        pooled_inputs = F.adaptive_avg_pool2d(x, 1).flatten(1)  # CondConv routing
+        routing_weights = torch.sigmoid(self.routing(pooled_inputs))
+        x = self.cond_conv(x, routing_weights)
+        return x
+
+class DynamicBlock(nn.Module):
+    default_act = nn.SiLU()  # default activation
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True, num_experts=4):
+        super().__init__()
+        self.conv = nn.Sequential(
+            DynamicConv_Single(c1, c2, kernel_size=k, stride=s, padding=autopad(k, p, d), dilation=d, groups=g, num_experts=num_experts),
+            nn.BatchNorm2d(c2),
+            self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+        )
+    
+    def forward(self, x):
+        return self.conv(x)
+
+class GhostModule(nn.Module):
+    def __init__(self, inp, oup, kernel_size=1, ratio=2, dw_size=3, stride=1, act_layer=nn.SiLU, num_experts=4):
+        super(GhostModule, self).__init__()
+        self.oup = oup
+        init_channels = math.ceil(oup / ratio)
+        new_channels = init_channels * (ratio - 1)
+
+        self.primary_conv = DynamicBlock(inp, init_channels, kernel_size, stride, num_experts=num_experts)
+
+        self.cheap_operation = DynamicBlock(init_channels, new_channels, dw_size, 1, g=init_channels, num_experts=num_experts)
+
+    def forward(self, x):
+        x1 = self.primary_conv(x)
+        x2 = self.cheap_operation(x1)
+        out = torch.cat([x1, x2], dim=1)
+        return out[:, :self.oup, :, :]
+
+class Bottleneck_Dynamic(Bottleneck):
+    def __init__(self, c1, c2, shortcut=True, g=1, k=(3, 3), e=0.5):
+        super().__init__(c1, c2, shortcut, g, k, e)
+        c_ = int(c2 * e)  # hidden channels
+        self.cv2 = DynamicBlock(c2, c2, 3)
+
+class C2f_Dynamic(C2f):
+    def __init__(self, c1, c2, n=1, shortcut=False, g=1, e=0.5):
+        super().__init__(c1, c2, n, shortcut, g, e)
+        self.m = nn.ModuleList(Bottleneck_Dynamic(self.c, self.c, shortcut, g, k=(3, 3), e=1.0) for _ in range(n))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
